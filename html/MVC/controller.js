8277220ee660e3150a443720e37b4289dd42e809
Beschieru Elena <34343470+elenabeschieru@users.noreply.github.com>
2019-10-25 12:10:54 +0300

    MVC

--- html/MVC/controller.js
+++ html/MVC/controller.js
@@ -1,29 +1,30 @@
 class t_eq2_controller{
-  view;
-  model;
 
-  constructor(view, model)
-  {
-   this.view = view;
-   this.model = model;
-   //construim o lista de evenimente care au atasate functii ascultatoare
-   var events = new t_events();
-   //adaugam on_solve (met constrollerului) in lista
-   events.add_event("solve_cliked_uab", this.on_solve);
-   // transmitem lista in view
-   this.view.set_events_list(events);
-  }
-  on_solve()
-  {
-    var a = this.view.get_a();
-    var b = this.view.get_b();
-    var c = this.view.get_c();
+    view;
+    model;
 
-    this.model.set_coef(a,b,c);
-    this.model.solve();
-    var solutions = this.model.get_solutions();
+    constructor(view,model){
+        this.view = view;
+        this.model = model; 
 
-    this.view.set_solutions_para(solutions);
-  }
+        var events = new t_events();
+
+        events.add_event("solve_clicked_uab",this.on_solve.bind(this));
+
+        this.view.set_events_list(events);
+    }
+    on_solve(){
+        var a = this.view.get_a();
+        var b = this.view.get_b();
+        var c = this.view.get_c();
+
+        this.model.set_coeficients(a,b,c);
+        this.model.solve();
+        var solutions = this.model.get_solutions();
+
+        this.view.set_solutions_para(solutions);
+    }
 }
-const app = new t_eq2_controller(new t_eq2_view(), new t_eq2_model());
+
+const app = new t_eq2_controller(new t_eq2_view(),new t_eq2_model());
+
--- html/MVC/events.js
+++ html/MVC/events.js
@@ -1,27 +1,23 @@
 class t_events{
+    events;
+    constructor(){
+        this.events = [];
+    }
 
-  events;
+    add_event(event_name,listener){
+        if(this.events.length == 0){
+            this.events.push(event_name);
+            this.events[event_name] = [];
+        }else{
+            if(this.events.indexOf(event_name) == -1){
+                this.events.push(event_name);
+                this.events[event_name] = [];
+            }
+        }
+        this.events[event_name].push(listener);
+    }
 
-  constructor()
-  {
-   this.events = [];
-  }
-  add_event(event_name, listener)
-  {
-   if(this.events_name.legth == 0)
-   {
-     this.events.push(event_name);
-     this.events[event_name] = [];
-   }else
-        if(this.events.indexOf(event_name)== -1){
-           this.events.push(eevnt_name);
-           this.events[event_name] = [];
-         }
-   this.events[event_name].push(listener);
-
-  }
-  call_event(event_name, args)
-  {
-     this.events[event_name].forEach(f => f(args) );
-  }
+    call_event(event_name,args){
+        this.events[event_name].forEach(f => f(args));
+    }
 }
