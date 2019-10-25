a86b9d42adcf990627b75f0def3e3aa4d0248383
Beschieru Elena <34343470+elenabeschieru@users.noreply.github.com>
2019-10-25 12:45:13 +0300

    new gyro

--- html/gyro/gyro.js
+++ html/gyro/gyro.js
@@ -1,6 +1,6 @@
 window.addEventListener("deviceorientation", on_rotate_uab);
 
-function on_roatate_uab(e)
+function on_rotate_uab(e)
 {
   document.getElementById("id_alpha").innerHTML = e.alpha;
   document.getElementById("id_beta").innerHTML = e.beta;
