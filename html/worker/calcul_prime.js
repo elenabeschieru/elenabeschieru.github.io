79b8efcf0cf92370cadd58bcdd7611e84a16d771
Beschieru Elena <34343470+elenabeschieru@users.noreply.github.com>
2019-11-22 13:20:52 +0200

    Add files via upload

--- html/worker/calcul_prime.js
+++ html/worker/calcul_prime.js
@@ -1,7 +1,21 @@
+var ruleaza = false;
+
+onmessage = function(e){
+  if(e.data=="start"){
+       ruleaza = true;
+  }
+  else
+     if(e.data=="stop") {
+       ruleaza = false;
+    }
+}
+
+
+
 function is_prime(n)
 {
-  for(var i = 2; i * i<=n; i++)
-  { 
+  for(var i = 2; i * i <=n; i++)
+  {
       if(n % i == 0)
         return true;
 
@@ -10,8 +24,25 @@ function is_prime(n)
   }
 }
 
-for(var i=1E11; i< 1E12; i++)
+function start_calcul(start_numar)
+{
+  if(ruleaza == false)
+  {
+       ;
+  }else{
+    for(var i=start_numar; i< start_numar+ 100 ; i++){
 
-    if(is_prime(i))
-    postMessage(i);
+      if(is_prime(i))
+        postMessage(i);
+        start_numar += 100;
+  }
+  }
+  if(start_numar < 1E12){
+    setTimeout(start_calcul, 1, start_numar);
+  }else 
+     postMessage("gata");
+}
+
+start_calcul(1E11);
+
 
--- html/worker/desenare.js
+++ html/worker/desenare.js
@@ -1,10 +1,11 @@
 document.getElementById('id_start').addEventListener("click", start);
 document.getElementById('id_stop').addEventListener("click", stop);
-document.getElementById('id_logic').innerHTML = "UI version = 2019.11.01.6";
+document.getElementById('id_logic').innerHTML = "UI version = 2019.11.22.0";
 
 var timer_1;
-var unghi = {};
-unghi.valoare = 0;
+var unghi = {valoare : 0};
+var muncitor = null;
+
 
 
 function  start()
@@ -14,18 +15,21 @@ function  start()
   var canvas = document.getElementById("id_canvas");
   var context = canvas.getContext("2d");
 
- 
+
    var raza_mare = 100;
    var raza_mica = 10;
-   var muncitor = new Worker("calcul_prime.js");
-   muncitor.onmessage = function (e) {
-     document.getElementById("id_prime").innerHTML = e.data;
-   }
-   
+   if(muncitor == null){
+      muncitor = new Worker("calcul_prime.js");
+      muncitor.onmessage = function (e) {
+      document.getElementById("id_prime").innerHTML = e.data;}
+  }
+   else
+   muncitor.postMessage("start");
+
     // desenam un cerc la pozitia data de unghi
     timer_1 = setInterval(desenare, 20, context, canvas, raza_mica, raza_mare, unghi);
 
-    
+
 }
 
 function stop()
@@ -33,6 +37,7 @@ function stop()
   document.getElementById("id_start").disabled = false;
   document.getElementById("id_stop").disabled = true;
   clearInterval(timer_1);
+  mincitor.postMessage("stop");
 }
 
 function desenare(context, canvas, raza_mica, raza_mare, unghi)
